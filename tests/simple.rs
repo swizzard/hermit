acf286e48f9c192cbf65470baa36dec851eae6c3
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-10-28 13:46:10 -0700

    Fix test paths.
    Was confused since test.toml was _also_ in the home directory. All paths in integration
    tests are relative to the crate root.
    
    Failing test:
    * add a provisioner.toml test file with some more interesting data structures
    
    We might consider skipping the more complex stuff at first.

--- tests/simple.rs
+++ tests/simple.rs
@@ -5,10 +5,15 @@ use hermit::*;
 #[test]
 fn test_count_kvs() {
 
-    let cases: Vec<(&str, usize)> = vec![("test.toml", 6)];
+    let cases: Vec<(&str, usize)> = vec![
+        ("tests/test.toml", 6),
+        ("tests/provisioner.toml", 6),
+    ];
     for case in cases {
         let raw = fs::read_to_string(case.0).expect("can't read file");
         let kvs = parse::parse_s(raw);
         assert_eq!(kvs.len(), case.1)
     }
 }
+
+
