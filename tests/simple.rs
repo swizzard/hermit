2288b63a52d4f2c8b5bddb6ca4a9f50ffd77dd70
Coleman McFarland <cmcfarland@paperlesspost.com>
2018-10-28 13:11:17 -0700

    Sketch out integration tests folder.
    Also add .travis.yml file that might just work.

---
+++ tests/simple.rs
@@ -0,0 +1,14 @@
+extern crate hermit;
+use std::fs;
+use hermit::*;
+
+#[test]
+fn test_count_kvs() {
+
+    let cases: Vec<(&str, usize)> = vec![("test.toml", 6)];
+    for case in cases {
+        let raw = fs::read_to_string(case.0).expect("can't read file");
+        let kvs = parse::parse_s(raw);
+        assert_eq!(kvs.len(), case.1)
+    }
+}
